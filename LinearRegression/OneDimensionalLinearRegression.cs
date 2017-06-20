12474f68c2c508b40d2e61b21cb0d0c9d1763d40
Victor <wangzhe@cn.ibm.com>
2017-06-20 21:58:48 +0800

    commit

---
+++ LinearRegression/OneDimensionalLinearRegression.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegression
+{
+    public class OneDimensionalLinearRegression
+    {
+        private double[] _sourceXData;
+        private double[] _sourceYData;
+        private double _xAverage;
+        private double _yAverage;
+
+        public OneDimensionalLinearRegression()
+        {
+
+        }
+
+        public double[] SourceXData
+        {
+            get
+            {
+                return _sourceXData;
+            }
+
+            set
+            {
+                _sourceXData = value;
+                double sum = 0;
+                foreach(double element in _sourceXData)
+                {
+                    sum += element;
+                }
+                _xAverage = sum / _sourceXData.Length;
+            }
+        }
+
+        public double[] SourceYData
+        {
+            get
+            {
+                return _sourceYData;
+            }
+
+            set
+            {
+                _sourceYData = value;
+                double sum = 0;
+                foreach (double element in _sourceYData)
+                {
+                    sum += element;
+                }
+                _yAverage = sum / _sourceYData.Length;
+            }
+        }
+
+        public double XAverage
+        {
+            get
+            {
+                return _xAverage;
+            }
+
+        }
+
+        public double YAverage
+        {
+            get
+            {
+                return _yAverage;
+            }
+        }
+
+        public void getLinearEquation(out double a, out double b)
+        {
+            a = 0;
+            b = 0;
+            double sumA = 0;
+            double sumB = 0;
+            for(int i = 0; i < _sourceXData.Length; i++)
+            {
+                sumA += (_sourceXData[i] - _xAverage) * (_sourceYData[i] - _yAverage);
+                sumB += (_sourceXData[i] - _xAverage) * (_sourceXData[i] - _xAverage);
+            }
+            b = sumA / sumB;
+            a = _yAverage - b * _xAverage;            
+        }
+
+    }
+}
---
+++ MachineLearningTest/MachineLearningTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearningTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearRegression.OneDimensionalLinearRegression clsA = new LinearRegression.OneDimensionalLinearRegression();
+            double[] x = { 3, 5.56, 8, 12, 13.3 };
+            double[] y = { 5, 8, 9, 11, 12.2 };
+            double a;
+            double b;
+            clsA.SourceXData = x;
+            clsA.SourceYData = y;
+            clsA.getLinearEquation(out a, out b);
+            Console.WriteLine("a={0}", a.ToString());
+            Console.WriteLine("b={0}", b.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
